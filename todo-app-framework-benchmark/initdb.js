1b58516a6322a80d8526ea625b3b035a3d78cf03
FDBC <fernando@dbce.fr>
2026-03-20 22:50:40 +0100

    init next and djaango sql lite database

--- todo-app-framework-benchmark/initdb.js
+++ todo-app-framework-benchmark/initdb.js
@@ -30,3 +30,35 @@ for (let i = 1; i <= 10000; i++) {
 }
 
 db.close();
+
+db = new DatabaseSync('./nextjs/todos.db');
+
+db.exec(`
+    DROP TABLE IF EXISTS todos;
+    CREATE TABLE todos(
+        todo TEXT
+    );
+`);
+
+for (let i = 1; i <= 10000; i++) {
+  const stmt = db.prepare('INSERT INTO todos (todo) VALUES (?)');
+  stmt.run(`Todo ${i}`);
+}
+
+db.close();
+
+db = new DatabaseSync('./django/todos.db');
+
+db.exec(`
+    DROP TABLE IF EXISTS todos;
+    CREATE TABLE todos(
+        todo TEXT
+    );
+`);
+
+for (let i = 1; i <= 10000; i++) {
+  const stmt = db.prepare('INSERT INTO todos (todo) VALUES (?)');
+  stmt.run(`Todo ${i}`);
+}
+
+db.close();
